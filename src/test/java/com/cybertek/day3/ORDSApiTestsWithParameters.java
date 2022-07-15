30e3c51d2b1cff0746c6dd6df3ef2b5c7413fa0f
AyseKoklu <rukoklu32@gmail.com>
2022-07-15 16:06:46 +0100

    ORDS TC1 and TC2 done

--- src/test/java/com/cybertek/day3/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cybertek/day3/ORDSApiTestsWithParameters.java
@@ -1,6 +1,10 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ORDSApiTestsWithParameters {
 
     @BeforeAll
-    public static void init(){
+    public static void init() {
         // save baseurl inside this variable so that we dont need to type each http method.
         baseURI = "http://54.159.143.93:1000/ords/hr";
     }
@@ -22,6 +26,39 @@ public class ORDSApiTestsWithParameters {
         And Payload should contain "United States of America"
      */
 
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1() {
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+    }
+
+    // Send a GET request to employees and get only employees who work as an IT_PROG
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2() {
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"job_id\":\"IT_PROG\"}")
+                .log().all()
+                .when()
+                .get("/employees");
+
+        assertEquals(200, response.statusCode());
+        assertEquals("application/json", response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        response.prettyPrint();
+    }
 
 
 }
