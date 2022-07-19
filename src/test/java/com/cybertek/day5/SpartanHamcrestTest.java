684dc778c3d668063a45fc96438f4f6a58e58ce1
AyseKoklu <rukoklu32@gmail.com>
2022-07-19 18:48:36 +0100

    day05 done

---
+++ src/test/java/com/cybertek/day5/SpartanHamcrestTest.java
@@ -0,0 +1,24 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class SpartanHamcrestTest extends SpartanTestBase {
+
+    @DisplayName("GET spartan/search and chaining together")
+    @Test
+    public void test1() {
+
+
+
+    }
+
+}
